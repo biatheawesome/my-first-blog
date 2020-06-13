c3afc986ac02ec0139c9232180df7a4adaaf8738
biatheawesome <biancafraunhoffer@gmail.com>
2020-06-13 10:32:33 +0100

    commit

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,3 @@
-from django.db import models
-
-# Create your models here.
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
@@ -10,10 +7,8 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
-
     def publish(self):
-    	self.published_date = timezone.now()
-    	self.save()
-    
+        self.published_date = timezone.now()
+        self.save()
     def __str__(self):
-    	return self.title
+        return self.title
