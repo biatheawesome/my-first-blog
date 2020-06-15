1a10c1de1823d71ce744303fdb9c612db231bb0b
biatheawesome <biancafraunhoffer@gmail.com>
2020-06-15 22:09:10 +0100

    trying to implement a search bar; also worked on the html and css

--- blog/forms.py
+++ blog/forms.py
@@ -5,3 +5,10 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+class SearchForm(forms.Form):
+    query=forms.CharField()
+
+    def clean_query(self):
+        c_query=self.cleaned_data['query']
+        return c_query
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
 from django.utils import timezone
 from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, SearchForm
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
@@ -38,3 +39,26 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_search(request):
+    form=SearchForm()
+    if request.method=='GET':
+        form=SearchForm(request.GET)
+        if form.is_valid():
+            query=request.GET.get('q',None)
+            if query:
+                posts= Post.objects.filter(title__icontains=query)
+            return render(request, 'blog/post_search.html', {'posts':posts, 'form':form})
+        else:
+            print(form.errors)
+            return render(request, 'blog/search_error.html', {})
+
+
+def get_blog_queryset(query=None):
+    queryset=[]
+    queries=query.split(" ")
+    for q in queries:
+        posts=Post.objects.filter(Q(title__icontains=q)|Q(text__icontains=q)).distinct()
+        for post in posts:
+            queryset.append(post)
+    return queryset
